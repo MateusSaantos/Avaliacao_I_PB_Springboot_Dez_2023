95288d683f1f504e5dce24058d3734a0b97368c2
Mateus Santos <mateus_saantos@outlook.com>
2023-12-27 19:18:55 -0300

    Criação do projeto e pacotes

---
+++ Avaliacao_I_PB_Springboot_Dez_2023_Exercicios/src/main/java/com/mycompany/avaliacao_i_pb_springboot_dez_2023_1_quiz/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.avaliacao_i_pb_springboot_dez_2023_1_quiz;
+
+/**
+ *
+ * @author Mateus Santos
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Avaliacao_I_PB_Springboot_Dez_2023_Exercicios/src/main/java/com/mycompany/avaliacao_i_pb_springboot_dez_2023_2_login/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.avaliacao_i_pb_springboot_dez_2023_2_login;
+
+/**
+ *
+ * @author Mateus Santos
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
